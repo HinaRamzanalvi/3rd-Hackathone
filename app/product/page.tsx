25bd31a132ae0c80cd5411d8c2fb8292a8016bee
Hina Alvi <princess4177812@gmail.com>
2025-01-19 03:29:17 -0800

    updated

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -3,7 +3,7 @@
 
 import { client } from "@/sanity/lib/client";
 import Image from "next/image";
-import Link from "next/link";
+
 import cartall from "@/components/public/cartall.svg";
 
 const getData = async () => {
